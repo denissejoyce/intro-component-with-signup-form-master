2f0356b7743c706e419edfef29bfb1997c0e3f22
denissejoyce <denisse.alido@gmail.com>
2024-06-12 01:09:58 -0500

    🚀 client-side form validation ready

--- js/script.js
+++ js/script.js
@@ -4,47 +4,60 @@ const btnTry = document.querySelector('.btn--try');
 const btnSubmit = document.querySelector('.btn--submit');
 const formQ1 = document.querySelector('.form__input');
 const alerts = document.querySelectorAll('.form__alert');
-let active;
+let active,
+  ctr = 0,
+  isValid = true;
 
 const getCurrent = function () {
   active = document.activeElement.id;
 };
 
 const checkInput = function () {
-  //   const userFname = document.getElementById('userFname').value;
-  //   const userLname = document.getElementById('userLname').value;
-  let userEmail = document.getElementById('userEmail').value;
-  let ctr = 0;
-  let inputElem = [];
-  //   const userPword = document.getElementById('userPword').value;
-
   const verifyEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  userEmail = verifyEmail.test(userEmail) ? userEmail : '';
-  //   const userInfo = [userFname, userLname, userEmail, userPword];
 
   const userInfo = {
     userFname: document.getElementById('userFname').value,
     userLname: document.getElementById('userLname').value,
-    userEmail: userEmail,
+    userEmail: document.getElementById('userEmail').value,
     userPword: document.getElementById('userPword').value,
   };
 
-  // EMPTY FORM
+  const info = Object.keys(userInfo);
+
   for (const info in userInfo) {
-    if (userInfo[info] != '') ctr += 1;
+    if (userInfo[info] == '') {
+      document.getElementById(info).classList.add('form__error');
+    } else {
+      if (info == 'userEmail') {
+        isValid = verifyEmail.test(userInfo[info]);
+        if (!isValid) {
+          document.getElementById(info).classList.add('form__error');
+        } else {
+          document.getElementById(info).classList.remove('form__error');
+        }
+      } else {
+        document.getElementById(info).classList.remove('form__error');
+      }
+    }
   }
-  if (!ctr) {
-    for (const user in userInfo) {
-      document.getElementById(user).classList.add('form__error');
-    }
-    for (let i = 0; i < alerts.length; i++) {
+  for (let i = 0; i < alerts.length; i++) {
+    if (document.getElementById(info[i]).classList.contains('form__error')) {
       alerts[i].classList.remove('invisible');
+      ctr += 1;
+    } else {
+      alerts[i].classList.add('invisible');
+      ctr -= 1;
     }
-  } else {
+    console.log(`ctr: ${ctr}`);
+  }
+  if (ctr <= 0) {
+    document.querySelector('.btn--submit').textContent = `Free trial claimed!`;
+    document
+      .querySelector('.btn--submit')
+      .classList.add('btn--submit--success');
   }
 };
 
-// TRY
 btnTry.addEventListener('click', function () {
   if (!active) {
     formQ1.focus();
@@ -55,9 +68,4 @@ btnTry.addEventListener('click', function () {
     }, 1000);
     document.getElementById(active).focus();
   }
-});
-
-btnSubmit.addEventListener('click', function () {
-  checkInput();
-  //   formQ1.classList.add('form__error');
 });
